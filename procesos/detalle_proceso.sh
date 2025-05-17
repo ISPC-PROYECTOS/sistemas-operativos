709050a5c8ce77f199ed1c50c6ff7f40b710bb48
Victoria Picco <vpicco1996@gmail.com>
2025-05-17 15:59:14 -0300

    Agregado script detalle_procesos.sh

---
+++ procesos/detalle_proceso.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Script para mostrar información detallada de un proceso específico
+read -p "Ingrese el PID del proceso: " pid
+echo "Información detallada del proceso:"
+ps -p "$pid" -o pid,ppid,user,%cpu,%mem,vsz,rss,tty,stat,start,time,comm
+echo
+echo "Presione Enter para volver al menú..."
+read -r dummy_var
+./procesos.sh 
