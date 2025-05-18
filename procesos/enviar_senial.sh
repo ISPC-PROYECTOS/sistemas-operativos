cf916c4b02f64252b5f01f40dea828cafcb46f81
PereyraHebe <p_hebe_c@hotmail.com>
2025-05-18 15:03:55 -0300

    Update enviar_senial.sh
    Se corrige opcion STOP, se agrega opcion CONT, y se agrega exit 0 para salir de la funcion una vez ejecutada.

--- procesos/enviar_senial.sh
+++ procesos/enviar_senial.sh
@@ -4,7 +4,7 @@ enviar_senal_pid() {
     echo"Enviar señales a procesos activos en el sistema."
     echo "Ingrese el PID del proceso al que desea enviar una señal:"
     read pid
-    echo "Ingrese la señal que desea enviar (ejemplo:15 (SIGTERM), 9 (SIGKILL), KILLSTOP:"
+    echo "Ingrese la señal que desea enviar (ejemplo:15 (SIGTERM), 9 (SIGKILL),  STOP (SIGSTOP), CONT (SIGCONT)):"
     read senal
 
     echo "¿Está seguro de que desea enviar la señal $senal al proceso con PID $pid? (s/n)"
@@ -14,10 +14,12 @@ enviar_senal_pid() {
                 kill -s "$senal" "$pid"
         if [[ $? -eq 0 ]]; then
             echo "Señal $senal enviada correctamente al proceso $pid."
+            exit 0
         else
             echo "Error al enviar la señal. Verifique el PID y la señal."
         fi
     else
         echo "Operación cancelada."
     fi
+    exit 0
 }
